7b0cfee7548022442a7dbf59b0bfb2113953c5bd
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-01-20 12:41:20 +0000

    Naming refactoring

--- src/main/scala/http4s/extend/syntax/package.scala
+++ src/main/scala/http4s/extend/syntax/package.scala
@@ -3,7 +3,7 @@ package http4s.extend
 package object syntax {
 
   object all extends AllSyntax
-  object naturalTransformation extends ByNameNaturalTransformationSyntax
+  object byNameNaturalTransformation extends ByNameNaturalTransformationSyntax
   object errorAdapt extends ErrorAdaptSyntax
   object monadError extends MonadErrorModuleSyntax
   object httpService extends Http4sServiceSyntax
