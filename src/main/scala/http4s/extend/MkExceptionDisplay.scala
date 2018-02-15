0f73089f14b3ec55b77a28c7aa61048bcf555f64
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-02-15 14:07:40 +0000

    Name refactoring

--- src/main/scala/http4s/extend/MkExceptionDisplay.scala
+++ src/main/scala/http4s/extend/MkExceptionDisplay.scala
@@ -1,6 +1,6 @@
 package http4s.extend
 
-object ExceptionDisplayImpl extends NewType {
+object MkExceptionDisplay extends NewType {
   def mk(b: String): T = b.asInstanceOf[T]
   def unMk(t: T): String = t.asInstanceOf[String]
   def mkF[F[_]](fs: F[String]): F[T] = fs.asInstanceOf[F[T]]
--- src/main/scala/http4s/extend/package.scala
+++ src/main/scala/http4s/extend/package.scala
@@ -4,6 +4,6 @@ package object extend {
 
   type ~~>[F[_], G[_]] = ByNameNt.~~>[F, G]
 
+  val ExceptionDisplay = MkExceptionDisplay
   type ExceptionDisplay = ExceptionDisplay.T
-  val ExceptionDisplay = ExceptionDisplayImpl
 }
