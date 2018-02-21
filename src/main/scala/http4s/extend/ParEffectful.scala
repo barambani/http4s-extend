7ca367ec06a57825de72a8027cfcfa29e17e46ac
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-02-21 01:26:16 +0000

    Fixed error in ParEffectful

--- src/main/scala/http4s/extend/ParEffectful.scala
+++ src/main/scala/http4s/extend/ParEffectful.scala
@@ -1,7 +1,9 @@
 package http4s.extend
 
-import cats.effect.IO
-import cats.{Apply, Semigroupal}
+import cats.Semigroup
+import cats.effect.{Effect, IO}
+import cats.syntax.apply._
+import cats.syntax.either._
 
 import scala.concurrent.ExecutionContext
 
@@ -12,12 +14,10 @@ import scala.concurrent.ExecutionContext
   */
 trait ParEffectful[F[_]] {
 
-  val semigroupalEvidence: Semigroupal[F]
-
-  def parMap2[A, B, R](fa: F[A], fb: F[B])(f: (A, B) => R): F[R]
+  def parMap2[A, B, R](fa: =>F[A], fb: =>F[B])(f: (A, B) => R): F[R]
 
   def parMap3[A, B, C, R](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => R): F[R] =
-    parMap2(fa, semigroupalEvidence.product(fb, fc))((a, b) => f(a, b._1, b._2))
+    parMap2(fa, parMap2(fb, fc)(Tuple2.apply))((a, bc) => f(a, bc._1, bc._2))
 
   def parTupled2[A, B, R](fa: F[A], fb: F[B]): F[(A, B)] =
     parMap2(fa, fb)(Tuple2.apply)
@@ -26,19 +26,28 @@ trait ParEffectful[F[_]] {
     parMap3(fa, fb, fc)(Tuple3.apply)
 }
 
-sealed trait ParEffectfulInstances {
+sealed trait ParEffectful1Instances {
 
-  implicit def ioEffectfulOp(implicit ec: ExecutionContext): ParEffectful[IO] =
+  implicit def ioEffectfulOp(implicit ev2: Semigroup[Throwable], ec: ExecutionContext): ParEffectful[IO] =
     new ParEffectful[IO] {
 
-      val semigroupalEvidence = Semigroupal[IO]
-      val composedApply = Apply[IO] compose Apply[IO]
+      val evidence = Effect[IO]
 
-      def parMap2[A, B, R](fa: IO[A], fb: IO[B])(f: (A, B) => R): IO[R] =
-        composedApply.map2(fs2.async.start(fa), fs2.async.start(fb))(f) flatMap identity
+      def parMap2[A, B, R](fa: =>IO[A], fb: =>IO[B])(f: (A, B) => R): IO[R] =
+        (fs2.async.start(fa), fs2.async.start(fb)) mapN {
+          (ioa, iob) =>
+            evidence.rethrow(
+              (ioa.attempt, iob.attempt) mapN {
+                case (Right(a), Right(b)) => f(a, b).asRight
+                case (Left(ea), Left(eb)) => ev2.combine(ea, eb).asLeft
+                case (Left(ea), _)        => ea.asLeft
+                case (_, Left(eb))        => eb.asLeft
+              }
+            )
+        } flatMap identity
     }
 }
 
-object ParEffectful extends ParEffectfulInstances {
+object ParEffectful extends ParEffectful1Instances {
   @inline def apply[F[_]](implicit F: ParEffectful[F]): ParEffectful[F] = F
 }
