76bc2f9f590f74dbdbcc3c886f07651c0647668a
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-03-15 19:05:17 +0000

    Refactored dependency version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,34 +2,36 @@ import sbt._
 
 object Dependencies {
 
-  private val http4sVersion     = "0.18.2"
-  private val monixVersion      = "3.0.0-M3"
-  private val scalazVersion     = "7.2.20"
-  private val catsLawsVersion   = "1.0.1"
+  object versionOf {
+    private[Dependencies] val http4s   = "0.18.2"
+    private[Dependencies] val monix    = "3.0.0-M3"
+    private[Dependencies] val scalaz   = "7.2.20"
+    private[Dependencies] val catsLaws = "1.0.1"
 
-  private val catsTestKitVersion  = "1.0.1"
-  private val scalaCheckVersion   = "1.13.5"
-  private val catsEffectLaws      = "0.9"
+    private[Dependencies] val catsTestKit     = "1.0.1"
+    private[Dependencies] val scalaCheck      = "1.13.5"
+    private[Dependencies] val catsEffectLaws  = "0.9"
 
-  private val kindProjectorVersion = "0.9.6"
+    private[Dependencies] val kindProjector = "0.9.6"
+  }
 
   val externalDependencies: Seq[ModuleID] = Seq(
-    "org.http4s"          %% "http4s-dsl"           % http4sVersion   withSources(),
-    "org.http4s"          %% "http4s-blaze-server"  % http4sVersion   withSources(),
-    "org.http4s"          %% "http4s-blaze-client"  % http4sVersion   withSources(),
-    "org.http4s"          %% "http4s-circe"         % http4sVersion   withSources(),
-    "io.monix"            %% "monix"                % monixVersion    withSources(),
-    "org.scalaz"          %% "scalaz-concurrent"    % scalazVersion   withSources(),
-    "org.typelevel"       %% "cats-laws"            % catsLawsVersion withSources()
+    "org.http4s"    %% "http4s-dsl"           % versionOf.http4s   withSources(),
+    "org.http4s"    %% "http4s-blaze-server"  % versionOf.http4s   withSources(),
+    "org.http4s"    %% "http4s-blaze-client"  % versionOf.http4s   withSources(),
+    "org.http4s"    %% "http4s-circe"         % versionOf.http4s   withSources(),
+    "io.monix"      %% "monix"                % versionOf.monix    withSources(),
+    "org.scalaz"    %% "scalaz-concurrent"    % versionOf.scalaz   withSources(),
+    "org.typelevel" %% "cats-laws"            % versionOf.catsLaws withSources()
   )
 
   val testDependencies: Seq[ModuleID] = Seq(
-    "org.typelevel"   %% "cats-testkit"     % catsTestKitVersion  % "test" withSources(),
-    "org.scalacheck"  %% "scalacheck"       % scalaCheckVersion   % "test" withSources(),
-    "org.typelevel"   %% "cats-effect-laws" % catsEffectLaws      % "test" withSources()
+    "org.typelevel"   %% "cats-testkit"     % versionOf.catsTestKit  % "test" withSources(),
+    "org.scalacheck"  %% "scalacheck"       % versionOf.scalaCheck   % "test" withSources(),
+    "org.typelevel"   %% "cats-effect-laws" % versionOf.catsEffectLaws      % "test" withSources()
   )
 
   val compilerPlugins: Seq[ModuleID] = Seq(
-    compilerPlugin("org.spire-math" %% "kind-projector" % kindProjectorVersion)
+    compilerPlugin("org.spire-math" %% "kind-projector" % versionOf.kindProjector)
   )
 }
