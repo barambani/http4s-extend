f5413db053904d981074d2c3bd4316435ae3f89c
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-03-18 15:33:57 +0000

    Added parTraverse based on parMap2

--- src/main/scala/http4s/extend/syntax/ErrorAdaptSyntax.scala
+++ src/main/scala/http4s/extend/syntax/ErrorAdaptSyntax.scala
@@ -6,7 +6,7 @@ trait ErrorAdaptSyntax {
   implicit def errorAdaptSyntax[F[_] : ErrorAdapt, A](anFa: => F[A]): ErrorAdaptOps[F, A] = new ErrorAdaptOps(anFa)
 }
 
-final class ErrorAdaptOps[F[_], A](anFa: => F[A])(implicit F: ErrorAdapt[F]) {
+private[syntax] final class ErrorAdaptOps[F[_], A](anFa: => F[A])(implicit F: ErrorAdapt[F]) {
   def attemptMapLeft[E](errM: Throwable => E): F[Either[E, A]] =
     F.attemptMapLeft(anFa)(errM)
 }
