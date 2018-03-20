c1339918cbc7cac2964aa7af775575029b266bac
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-03-20 21:24:34 +0000

    Updated versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,33 +2,32 @@ import sbt._
 
 object Dependencies {
 
-  object versionOf {
-    private[Dependencies] val http4s   = "0.18.2"
-    private[Dependencies] val monix    = "3.0.0-M3"
-    private[Dependencies] val scalaz   = "7.2.20"
-    private[Dependencies] val catsLaws = "1.0.1"
+  private[Dependencies] object versionOf {
+    val http4s  = "0.18.3"
+    val monix   = "3.0.0-M3"
+    val scalaz  = "7.2.20"
+    val cats    = "1.1.0"
 
-    private[Dependencies] val catsTestKit     = "1.0.1"
-    private[Dependencies] val scalaCheck      = "1.13.5"
-    private[Dependencies] val catsEffectLaws  = "0.10"
+    val scalaCheck      = "1.13.5"
+    val catsEffectLaws  = "0.10"
 
-    private[Dependencies] val kindProjector = "0.9.6"
+    val kindProjector = "0.9.6"
   }
 
   val externalDependencies: Seq[ModuleID] = Seq(
-    "org.http4s"    %% "http4s-dsl"           % versionOf.http4s   withSources(),
-    "org.http4s"    %% "http4s-blaze-server"  % versionOf.http4s   withSources(),
-    "org.http4s"    %% "http4s-blaze-client"  % versionOf.http4s   withSources(),
-    "org.http4s"    %% "http4s-circe"         % versionOf.http4s   withSources(),
-    "io.monix"      %% "monix"                % versionOf.monix    withSources(),
-    "org.scalaz"    %% "scalaz-concurrent"    % versionOf.scalaz   withSources(),
-    "org.typelevel" %% "cats-laws"            % versionOf.catsLaws withSources()
-  )
+    "org.http4s"    %% "http4s-dsl"           % versionOf.http4s,
+    "org.http4s"    %% "http4s-blaze-server"  % versionOf.http4s,
+    "org.http4s"    %% "http4s-blaze-client"  % versionOf.http4s,
+    "org.http4s"    %% "http4s-circe"         % versionOf.http4s,
+    "io.monix"      %% "monix"                % versionOf.monix,
+    "org.scalaz"    %% "scalaz-concurrent"    % versionOf.scalaz,
+    "org.typelevel" %% "cats-laws"            % versionOf.cats
+  ) map (_.withSources)
 
   val testDependencies: Seq[ModuleID] = Seq(
-    "org.typelevel"   %% "cats-testkit"     % versionOf.catsTestKit  % "test" withSources(),
-    "org.scalacheck"  %% "scalacheck"       % versionOf.scalaCheck   % "test" withSources(),
-    "org.typelevel"   %% "cats-effect-laws" % versionOf.catsEffectLaws      % "test" withSources()
+    "org.typelevel"   %% "cats-testkit"     % versionOf.cats            % "test",
+    "org.scalacheck"  %% "scalacheck"       % versionOf.scalaCheck      % "test",
+    "org.typelevel"   %% "cats-effect-laws" % versionOf.catsEffectLaws  % "test"
   )
 
   val compilerPlugins: Seq[ModuleID] = Seq(
--- src/main/scala/http4s/extend/ByNameNt.scala
+++ src/main/scala/http4s/extend/ByNameNt.scala
@@ -7,9 +7,9 @@ import cats.effect.IO
 import cats.syntax.either._
 import monix.eval.{Task => MonixTask}
 import monix.execution.Scheduler
-
-import scala.concurrent.{ExecutionContext, Future}
 import scalaz.concurrent.{Task => ScalazTask}
+
+import scala.concurrent.Future
 
 sealed trait ByNameNt[F[_], G[_]] {
 
@@ -21,20 +21,20 @@ sealed trait ByNameNt[F[_], G[_]] {
 
 sealed trait ByNameNtInstances {
 
-  implicit def futureToIo(implicit ec: ExecutionContext): ByNameNt[Future, IO] =
+  implicit def futureToIo: ByNameNt[Future, IO] =
     new ByNameNt[Future, IO] {
-      def apply[A](fa: => Future[A]): IO[A] =
+      def apply[A](fa: =>Future[A]): IO[A] =
         IO.fromFuture(IO.eval(always(fa)))
     }
 
   implicit def monixTaskToIo(implicit s: Scheduler): ByNameNt[MonixTask, IO] =
     new ByNameNt[MonixTask, IO] {
-      def apply[A](fa: => MonixTask[A]): IO[A] = fa.toIO
+      def apply[A](fa: =>MonixTask[A]): IO[A] = fa.toIO
     }
 
   implicit def scalazTaskToIo: ByNameNt[ScalazTask, IO] =
     new ByNameNt[ScalazTask, IO] {
-      def apply[A](fa: => ScalazTask[A]): IO[A] =
+      def apply[A](fa: =>ScalazTask[A]): IO[A] =
         Eval.always(
           Either.catchNonFatal(fa.unsafePerformSync).fold(
             e => IO.raiseError[A](e),
