510cc9c25c8ad603f1d42487a5da105122ddbe69
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-03-30 13:55:21 +0100

    Updated docs and circe utils

--- src/main/scala/http4s/extend/util/CirceModule.scala
+++ src/main/scala/http4s/extend/util/CirceModule.scala
@@ -1,18 +1,36 @@
 package http4s.extend.util
 
+import cats.Show
 import cats.syntax.either._
 import io.circe.{Decoder, Encoder}
 
 object CirceModule {
 
-  def encoderFor[A](f: A => String): Encoder[A] =
-    Encoder.encodeString.contramap[A](f)
+  /**
+    * Gives a Circe Encoder for the type `A` when a `Show` instance
+    * is available for it
+    *
+    * @return An encoder for `A`
+    */
+  def encoderFor[A](implicit ev: Show[A]): Encoder[A] =
+    Encoder.encodeString.contramap[A](ev.show)
 
-  def decoderFor[A, B](f: String => A): Decoder[A] =
-    decoderMapFor(f)(identity)
+  /**
+    * Gives a Circe Decoder for the type `A` when a way to go from String to `A`
+    * is provided
+    *
+    * @return A decoder for `A`
+    */
+  def decoderFor[A]: (String => A) => Decoder[A] =
+    f => mappedDecoderFor(f)(identity)
 
-  def decoderMapFor[A, B](f: String => A)(g: A => B): Decoder[B] =
-    Decoder.decodeString emap {
-      str => Either.catchNonFatal(f(str)) leftMap (_ => s"Cannot parse $str to Long") map g
+  /**
+    * Gives a Circe Decoder for `A` that maps the successful decoded value with `f`
+    *
+    * @return A decoder for `A` that maps the result to `B` in case of successful decoding
+    */
+  def mappedDecoderFor[A, B]: (String => A) => (A => B) => Decoder[B] =
+    ff => f => Decoder.decodeString emap {
+      str => Either.catchNonFatal[A](ff(str)) leftMap (_ => s"Cannot parse $str to Long") map f
     }
 }
