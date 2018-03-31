429174d577ef95bd71a74f72575d9f08f86edde4
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-03-31 14:30:17 +0100

    Added Functor constraint to Natural Transformation

--- src/test/scala/http4s/extend/test/ByNameNtDiscipline.scala
+++ src/test/scala/http4s/extend/test/ByNameNtDiscipline.scala
@@ -1,37 +1,7 @@
-//package http4s.extend.test
-//
-//import cats.effect.IO
-//import cats.effect.laws.discipline.ConcurrentEffectTests
-//import cats.effect.laws.discipline.arbitrary._
-//import cats.effect.laws.util.TestContext
-//import cats.instances.double._
-//import cats.instances.either._
-//import cats.instances.int._
-//import cats.instances.string._
-//import cats.instances.tuple._
-//import cats.instances.unit._
-//import http4s.extend.ByNameNt._
-//import http4s.extend.test.Fixtures.MinimalSuite
-//import org.scalacheck.Arbitrary.arbDouble
-//
-//import scala.concurrent.Future
-//
-//
-//
-//final class ByNameNtDiscipline  extends MinimalSuite {
-//
-//  implicit val C = TestContext()
-//
-////  def bbbb: ScalazTask[Int] = ???
-////  def cccc: MonixTask[Double] = ???
-//
-//  {
-//    def aaaa[A](): Future[A] = ???
-//    implicit def io[A](implicit ev: Future ~~> IO): IO[A] = ev(aaaa())
-//
-//    checkAll(
-//      "ConcurrentEffect[IO]",
-//      ConcurrentEffectTests[IO].concurrentEffect[String, Int, Double]
-//    )
-//  }
-//}
+package http4s.extend.test
+
+
+
+final class ByNameNtDiscipline {
+
+}
