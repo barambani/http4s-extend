6bc37101c82529cfacf223abec7a8efac1191274
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-04-09 23:41:07 +0100

    Updated versions, added comments, moved Eq instance in the newtype object

--- src/main/scala/http4s/extend/MkExceptionDisplay.scala
+++ src/main/scala/http4s/extend/MkExceptionDisplay.scala
@@ -1,7 +1,13 @@
 package http4s.extend
 
+import cats.Eq
+import cats.instances.string._
+
 object MkExceptionDisplay extends NewType {
   def mk(b: String): T = b.asInstanceOf[T]
   def unMk(t: T): String = t.asInstanceOf[String]
   def mkF[F[_]](fs: F[String]): F[T] = fs.asInstanceOf[F[T]]
+
+  implicit val throwableCompleteMessageEq: Eq[ExceptionDisplay] =
+    Eq.by[ExceptionDisplay, String](unMk)
 }
