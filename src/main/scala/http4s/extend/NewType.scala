5580fafd552f00a243483af81366db4ea1db4f17
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-04-10 00:12:41 +0100

    Minor refactoring

--- src/main/scala/http4s/extend/NewType.scala
+++ src/main/scala/http4s/extend/NewType.scala
@@ -1,6 +1,6 @@
 package http4s.extend
 
-private [extend] trait NewType { self =>
+trait NewType { self =>
   private [extend] type Base
   private [extend] trait Tag extends Any
   type T <: Base with Tag
