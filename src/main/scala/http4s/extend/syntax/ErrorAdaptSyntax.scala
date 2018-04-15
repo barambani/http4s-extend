10d70f1e6ab46f676187b9724415b0eee1b46e71
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-04-15 20:40:32 +0100

    Improved error message for syntax

--- src/main/scala/http4s/extend/syntax/ErrorAdaptSyntax.scala
+++ src/main/scala/http4s/extend/syntax/ErrorAdaptSyntax.scala
@@ -3,10 +3,10 @@ package http4s.extend.syntax
 import http4s.extend.ErrorAdapt
 
 private[syntax] trait ErrorAdaptSyntax {
-  implicit def errorAdaptSyntax[F[_] : ErrorAdapt, A](anFa: => F[A]): ErrorAdaptOps[F, A] = new ErrorAdaptOps(anFa)
+  implicit def errorAdaptSyntax[F[_], A](anFa: =>F[A]): ErrorAdaptOps[F, A] = new ErrorAdaptOps(anFa)
 }
 
-private[syntax] final class ErrorAdaptOps[F[_], A](anFa: => F[A])(implicit F: ErrorAdapt[F]) {
-  def attemptMapLeft[E](errM: Throwable => E): F[Either[E, A]] =
+private[syntax] final class ErrorAdaptOps[F[_], A](anFa: =>F[A]) {
+  def attemptMapLeft[E](errM: Throwable => E)(implicit F: ErrorAdapt[F]): F[Either[E, A]] =
     F.attemptMapLeft(anFa)(errM)
 }
