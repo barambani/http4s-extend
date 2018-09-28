8978c40f083b5437c410b627a2687fc6859f7310
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-09-28 15:42:29 +0100

    Formatting

--- src/main/scala/http4s/extend/newtype.scala
+++ src/main/scala/http4s/extend/newtype.scala
@@ -5,9 +5,14 @@ trait newtype[A] {
   private[newtype] trait Tag extends Any
   type T <: Base with Tag
 
-  final def apply(a: A): T              = a.asInstanceOf[T]
-  final def mkF[F[_]](fa: F[A]): F[T]   = fa.asInstanceOf[F[T]]
-  final def unMkF[F[_]](ft: F[T]): F[A] = mkF[λ[α => F[α] => F[A]]](identity)(ft)
+  final def apply(a: A): T =
+    a.asInstanceOf[T]
+
+  final def mkF[F[_]](fa: F[A]): F[T] =
+    fa.asInstanceOf[F[T]]
+
+  final def unMkF[F[_]](ft: F[T]): F[A] =
+    mkF[λ[α => F[α] => F[A]]](identity)(ft)
 }
 
 object newtype {
