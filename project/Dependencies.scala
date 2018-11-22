8c8bfec0986781b70f19f1f5fb93b02467468a45
Scala steward <me@scala-steward.org>
2018-11-22 19:03:59 +0100

    Update kind-projector to 0.9.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
     val scalaCheck      = "1.13.5"
 
-    val kindProjector = "0.9.8"
+    val kindProjector = "0.9.9"
     val splain        = "0.3.4"
     val silencer      = "1.2.1"
   }
