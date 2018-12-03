4777203dd1b9776663ae5c110d605bcfa69ed291
Scala steward <me@scala-steward.org>
2018-12-03 13:26:12 +0100

    Update silencer-lib, silencer-plugin to 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
     val kindProjector = "0.9.9"
     val splain        = "0.3.4"
-    val silencer      = "1.2.1"
+    val silencer      = "1.3.0"
   }
 
   val externalDependencies: Seq[ModuleID] = Seq(
